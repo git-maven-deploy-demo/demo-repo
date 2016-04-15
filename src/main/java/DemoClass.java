108082d8f8b55817924eacd461ea3feffd96cec5
tm12tin <tm12tin@cs.umu.se>
2016-04-15 09:59:00 +0200

    added basic maven structure

---
+++ src/main/java/DemoClass.java
@@ -0,0 +1,14 @@
+/**
+ * Created by tm12tin on 2016-04-15.
+ */
+public class DemoClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello sword");
+
+
+    }
+
+
+}
---
+++ src/test/java/DemoClassTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by tm12tin on 2016-04-15.
+ */
+public class DemoClassTest {
+
+    @org.junit.Test
+    public void testMain() throws Exception {
+
+        assertTrue(false);
+
+    }
+}
